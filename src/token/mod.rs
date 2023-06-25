903ff677e913993d63c8d4585fa4267c3cdbd2fa
0xcacti <0xcacti@gmail.com>
2023-06-25 02:59:56 -0400

    add display trait to tokne

--- src/token/mod.rs
+++ src/token/mod.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[allow(dead_code)]
 #[derive(Debug, PartialEq)]
 pub enum Token {
@@ -36,4 +38,38 @@ pub enum Token {
     If,
     Else,
     Return,
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        return match self {
+            Token::Illegal => write!(f, "Illegal"),
+            Token::Eof => write!(f, "Eof"),
+            Token::Ident(s) => write!(f, "Ident({})", s),
+            Token::Int(s) => write!(f, "Int({})", s),
+            Token::Assign => write!(f, "Assign"),
+            Token::Plus => write!(f, "Plus"),
+            Token::Dash => write!(f, "Dash"),
+            Token::Bang => write!(f, "Bang"),
+            Token::Asterisk => write!(f, "Asterisk"),
+            Token::Slash => write!(f, "Slash"),
+            Token::Lt => write!(f, "LessThan"),
+            Token::Gt => write!(f, "GreaterThan"),
+            Token::Eq => write!(f, "Equal"),
+            Token::NotEq => write!(f, "NotEqual"),
+            Token::Comma => write!(f, "Comma"),
+            Token::Semicolon => write!(f, "Semicolon"),
+            Token::Lparen => write!(f, "Lparen"),
+            Token::Rparen => write!(f, "Rparen"),
+            Token::Lbrace => write!(f, "Lbrace"),
+            Token::Rbrace => write!(f, "Rbrace"),
+            Token::Function => write!(f, "Function"),
+            Token::Let => write!(f, "Let"),
+            Token::True => write!(f, "True"),
+            Token::False => write!(f, "False"),
+            Token::If => write!(f, "If"),
+            Token::Else => write!(f, "Else"),
+            Token::Return => write!(f, "Return"),
+        };
+    }
 }

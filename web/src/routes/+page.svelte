ab3d0d9024c217d78f88e356b177d09fdf9ed067
0xcacti <0xcacti@gmail.com>
2023-09-12 01:53:57 -0400

    create a very simple website with svelte - I will add the logic tomorrow

---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<head>
+    <title>Monkey Playground</title>
+</head>
+
+<body>
+    <header class="li-page-header">
+        <div class="container">
+            <div class="row">
+                <div class="sixteen columns">
+                    <div class="li-brand li-left">Monkey Playground</div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="container">
+        <div class="row">
+            <div class="sixteen columns">
+                <p class="run_button">
+                    <button id="run">Run</button>
+                </p>
+                <div class="editor">
+                    <h3>Editor</h3>
+                    <textarea rows="10" id="source" />
+                </div>
+                <div class="console">
+                    <h3>Output</h3>
+                    <pre><code id="output" /></pre>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+

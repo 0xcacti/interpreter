2601fbfc88cc3ec90a0007d4ed58644d84e0f3b6
0xcacti <0xcacti@gmail.com>
2023-09-12 21:16:17 -0400

    svelte is kinda cool

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,34 +1,50 @@
-<head>
-    <title>Monkey Playground</title>
-</head>
+<script>
+    import "../app.css";
+    
+        function handleSubmit() {
+        // prevent form from submitting traditionally
+        event.preventDefault();
+        alert("Button was clicked!");
+    }
+</script>
 
-<body>
-    <header class="li-page-header">
-        <div class="container">
-            <div class="row">
-                <div class="sixteen columns">
-                    <div class="li-brand li-left">Monkey Playground</div>
-                </div>
-            </div>
-        </div>
+<svelte:head>
+    <title>Monkey Playground</title>
+</svelte:head>
+
+<div class="bg-white m-10">
+    <header class="flex w-full mb-5">
+        <h1 class="text-4xl">Monkey Playground</h1>
     </header>
 
-    <div class="container">
-        <div class="row">
-            <div class="sixteen columns">
-                <p class="run_button">
-                    <button id="run">Run</button>
-                </p>
-                <div class="editor">
-                    <h3>Editor</h3>
-                    <textarea rows="10" id="source" />
+    <div class="flex w-full">
+        <form class="w-full">
+            <div class="mb-4 border border-black border-2 rounded-lg h-fit">
+                <div class="p-4 bg-white rounded-t-lg">
+                    <textarea
+                        id="comment"
+                        rows={4}
+                        class="w-full px-8 py-6 text-xl border-black focus:shadow-soft-primary-outline appearance-none rounded-lg border-2 border-solid"
+                        placeholder="monkey code goes here"
+                    />
                 </div>
-                <div class="console">
-                    <h3>Output</h3>
-                    <pre><code id="output" /></pre>
+                    <div
+                        class="flex items-center justify-between px-3 py-2 border-black border-t-2 space-x-5"
+                    >
+                        <div class="flex"></div>
+                        <button
+                            type="submit"
+                            class="inline-flex items-center py-2.5
+                            px-4 font-rounded font-bold text-center text-black rounded-lg
+                            bg-green-400 hover:ring-4 hover:ring-fuchsia-300 focus:ring-4
+                            focus:ring-fuchsia-300"
+                        >
+                            Submit
+                        </button>
+                    </div>
                 </div>
-            </div>
-        </div>
+        </form>
     </div>
-</body>
-
+</div>
+<!-- This is where the content of any nested route will be rendered -->
+<slot />

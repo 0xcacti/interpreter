61fbbd7c8f2ea2981f74815123a10b811c968e80
0xcacti <0xcacti@gmail.com>
2023-11-22 18:13:40 -0500

    start implementing op, but I am a little confused

---
+++ src/code.rs
@@ -0,0 +1,10 @@
+pub struct Definition {
+    pub name: string,
+    pub name: operand_widths: 
+}
+pub enum Opcode {
+    OpConstant(1),
+}
+
+pub type Instructions = Vec<Opcode>;
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod code;
 pub mod evaluator;
 pub mod lexer;
 pub mod parser;

18e61c93f33ed692c7558350e25d0584d19b2253
0xcacti <0xcacti@gmail.com>
2023-12-01 21:30:35 -0500

    rust is so annoying

--- src/code.rs
+++ src/code.rs
@@ -1,3 +1,49 @@
 pub type Opcode = u8;
 
 pub type Instructions = Vec<Opcode>;
+
+pub struct Definition {
+    pub name: String,
+    pub operand_widths: Vec<u64>,
+}
+
+pub enum Opcodes {
+    Constant,
+}
+
+impl Opcodes {
+    pub fn definition(&self) -> Definition {
+        match self {
+            Opcodes::Constant => {
+                return Definition {
+                    name: "OpConstant".to_string(),
+                    operand_widths: vec![2],
+                }
+            }
+        }
+    }
+}
+
+pub fn make(op: Opcode, operands: Vec<u64>) -> Instructions {
+    let definition = op.definition()
+    let length = op.
+
+}
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn check(opcode: Opcode, operands: Vec<u64>, expected: Vec<u8>) {
+        let instruction = make(opcode, operands);
+        assert_eq!(instruction.len(), expected.len());
+        for (i, b) in expected.iter().enumerate() {
+            assert_eq!(instruction[i], *b);
+        }
+    }
+
+    #[test]
+    fn it_makes_correctly() {
+        check(0, vec![0], vec![0]);
+        assert!(false);
+    }
+}
